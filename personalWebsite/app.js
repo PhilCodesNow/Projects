d9a8abc2b653ff33d2d5dbc2a16e1d9a09254faf
PhilCodesNow <philipmayojr@outlook.com>
2020-05-19 23:38:26 -0400

    worked on carousel more

---
+++ personalWebsite/app.js
@@ -0,0 +1,31 @@
+let carouselIndex = 0;
+
+function plusSlides(n){
+    showSlides(carouselIndex += n);
+}
+
+let slides = document.getElementsByClassName('slide');
+
+
+
+function showSlides(n){
+    console.log('length' + slides.length)
+    console.log('n' + n)
+    if(n > slides.length){
+        slideIndex = 0;
+    }
+    if(n < 0){
+        slideIndex = slides.length;
+ }
+    for(let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"
+    }
+    // slides[carouselIndex].style.display = "none";
+    slides[carouselIndex].style.display = "block";
+    
+}
+
+
+
+
+showSlides(carouselIndex)
