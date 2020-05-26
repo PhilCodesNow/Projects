ef89216f33c842fbf706eee1d974fe2d8b9f91cb
PhilCodesNow <philipmayojr@outlook.com>
2020-05-26 16:06:50 -0400

    fixed carouself bug

--- personalWebsite/app.js
+++ personalWebsite/app.js
@@ -11,18 +11,17 @@ let slides = document.getElementsByClassName('slide');
 function showSlides(n){
     console.log(slides)
     console.log('length' + slides.length)
-    console.log('n' + n)
-    if(n > slides.length){
-        slideIndex = 0;
-    }else if(n < 0){
-        slideIndex = slides.length;
+    console.log('carouselIndex = ' + carouselIndex)
+    if(carouselIndex > slides.length - 1){
+        carouselIndex = 0;
+    }else if(carouselIndex < 0){
+        carouselIndex = slides.length - 1;
  }
     for(let i = 0; i < slides.length; i++){
         slides[i].style.display = "none"
     }
     // slides[carouselIndex].style.display = "none";
     slides[carouselIndex].style.display = "block";
-    
 }
 
 
